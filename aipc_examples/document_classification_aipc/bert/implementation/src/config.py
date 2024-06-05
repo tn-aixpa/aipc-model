6cc5c3596916366e421028048bb08d08b835254d
Albana Celepija <acelepija@fbk.eu>
2024-06-05 15:56:21 +0000

    feat(classification) read secrets from config ini

---
+++ aipc_examples/document_classification_aipc/bert/implementation/src/config.py
@@ -0,0 +1,13 @@
+from configparser import ConfigParser
+
+def get_params(type):
+    parameters = {}
+    parser = ConfigParser()
+    parser.read("config.ini")
+    if parser.has_section(type):
+        parameters = {
+            item[0] : item[1]
+            for item in parser.items(type)
+        }
+    return parameters
+
