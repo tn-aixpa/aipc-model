35aa44badad4156a2d36502e7e5d5ee3ee4dbc56
Albana Celepija <acelepija@fbk.eu>
2024-06-17 15:26:53 +0000

    fix(bike-sharing)load model card metadata during the training function

--- aipc_examples/bike_sharing_aipc/implementation/src/functions/train_model.py
+++ aipc_examples/bike_sharing_aipc/implementation/src/functions/train_model.py
@@ -5,12 +5,22 @@ from zipfile import ZipFile
 from sklearn import ensemble
 
 @mlrun.handler()
-def train_model(context, bikesharing_dataitem: mlrun.DataItem):
+def train_model(context, bikesharing_dataitem: mlrun.DataItem, model_metadata):
     reference = bikesharing_dataitem.as_df()
     target = 'target'
     numerical_features = ['temp', 'atemp', 'hum', 'windspeed', 'hr', 'weekday']
     categorical_features = ['season', 'holiday', 'workingday']
-    regressor = ensemble.RandomForestRegressor(random_state = 0, n_estimators = 50)
+    
+    params = model_metadata["parameters"]
+    random_state = None
+    n_estimators = None
+    for param in params:
+        if param["name"] == "random_state":
+            random_state = param["value"]
+        if param["name"] == "n_estimators":
+            n_estimators = param["value"]
+    print(n_estimators)
+    regressor = ensemble.RandomForestRegressor(random_state = random_state, n_estimators = n_estimators)
     regressor.fit(reference[numerical_features + categorical_features], reference[target])
     joblib.dump(regressor, "bike_sharing_model.joblib")
     with ZipFile("bike_sharing_model.zip", "w") as z:
@@ -19,8 +29,8 @@ def train_model(context, bikesharing_dataitem: mlrun.DataItem):
     context.log_model(
         "bike_sharing_model",
         parameters={
-            "random_state": 0,
-            "n_estimators": 50
+            "random_state": random_state,
+            "n_estimators": n_estimators
         },
         model_file="bike_sharing_model.zip",
         labels={"class": "ensemble.RandomForestRegressor("},
